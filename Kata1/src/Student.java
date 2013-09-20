a0060bc216d62fd76ec2bd20d283cf9e9c52ab89
Lidia <Lidia@10.230.168.14>
2013-09-20 11:48:26 +0100

    Kata1

---
+++ Kata1/src/Student.java
@@ -0,0 +1,64 @@
+
+import java.util.Date;
+
+public class Student {
+
+    private String name;
+    private Date birthdate;
+
+    private Student(String name, Date birthdate) {
+        this.name = name;
+        this.birthdate = birthdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public int getAge(Date birthdate) {
+        return (int) 
+                getYearsInDays(
+                getDaysInHours(
+                getHoursInMinutes(
+                getMinutesInSeconds(
+                getSecondsInMilliseconds(
+                    today().getTime() - birthdate.getTime()
+                )))));
+    }
+
+    private Date today() {
+        return new Date();
+    }
+
+    private long getSecondsInMilliseconds(long milliseconds) {
+        return milliseconds / 1000;
+    }
+
+    private long getMinutesInSeconds(long seconds) {
+        return seconds / 60;
+    }
+
+    private long getHoursInMinutes(long minutes) {
+        return minutes / 60;
+    }
+
+    private long getDaysInHours(long hours) {
+        return hours / 24;
+    }
+
+    private double getYearsInDays(long days) {
+        return days / 365.25;
+    }
+}
